789d7427bf4c3181f0da0556807e1ef04c89cc26
centrattic <pialityagi@gmail.com>
2025-07-03 01:53:48 +0100

    print debugs

--- attention-probes/src/main.py
+++ attention-probes/src/main.py
@@ -2,12 +2,18 @@ from extract import Extractor, Comp
 from utils_data import DataLoader
 from utils_probe import LogisticRegressionProbe, MassMeanProbe
 
+print("Starting")
 ext  = Extractor("gpt2-medium")
+print("Extraction initialized")
 data = DataLoader("7_hist_fig_ispolitician")
+print("Data loaded")
 
 Xtr, ytr, Xte, yte = data.features(ext, layer=8, component=Comp.resid_post, agg="mean")
+print("Activations retrieved")
 probe_lg = LogisticRegressionProbe().fit(Xtr, ytr)
 probe_mm = MassMeanProbe().fit(Xtr, ytr)
+print("Probes trained")
 
 print(probe_lg.score(Xte, yte))
 print(probe_mm.score(Xte, yte))
+print("Done")
